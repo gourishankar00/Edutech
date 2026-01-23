509839a1a7464f54d41f6cc915f8179ab249e3fc
gourishankar00 <dummy@gmail.com>
2026-01-23 12:46:08 +0000

    solve initial commit

--- server/src/main/java/com/edutech/progressive/service/impl/CourseServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CourseServiceImplJpa.java
@@ -1,5 +1,50 @@
 package com.edutech.progressive.service.impl;
 
-public class CourseServiceImplJpa  {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edutech.progressive.entity.Course;
+import com.edutech.progressive.repository.CourseRepository;
+import com.edutech.progressive.service.CourseService;
+
+@Service
+public class CourseServiceImplJpa implements CourseService {
+    private CourseRepository courseRepository;
+
+    @Autowired
+    public CourseServiceImplJpa(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @Override
+    public List<Course> getAllCourses() throws Exception {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public Course getCourseById(int courseId) throws Exception {
+        return courseRepository.findByCourseId(courseId);
+    }
+
+    @Override
+    public Integer addCourse(Course course) throws Exception {
+        return courseRepository.save(course).getCourseId();
+    }
+
+    @Override
+    public void updateCourse(Course course) throws Exception {
+        courseRepository.save(course);
+
+    }
+
+    @Override
+    public void deleteCourse(int courseId) throws Exception {
+        courseRepository.deleteById(courseId);
+    }
+
+    public List<Course> getAllCourseByTeacherId(int teacherId) {
+        return courseRepository.findAllByTeacherId(teacherId);
+    }
 }
